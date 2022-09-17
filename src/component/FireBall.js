5e32a0a4b22f96b5353a7573362aece71fa4bafe
karuiner <intaekgong@gmail.com>
2022-09-17 22:39:54 +0900

    폭발효과를 적용하였으나 미흡함.

--- src/component/FireBall.js
+++ src/component/FireBall.js
@@ -3,11 +3,25 @@ import styled from "styled-components";
 
 function FireBall({ time = 1, rnd }) {
   let [check, setCheck] = useState(false);
+  let [check2, setCheck2] = useState(false);
+  let [ptc, setPtc] = useState([]);
+
   useEffect(() => {
+    if (ptc.length === 0) {
+      let sub = [];
+      for (let i = 0; i < 32; i++) {
+        let k = Math.random() * Math.PI * 2;
+        sub.push(k);
+      }
+      setPtc(sub);
+    }
+
     setTimeout(() => {
       setCheck(true);
       return () => {
-        setTimeout(() => {}, time * 2000);
+        setTimeout(() => {
+          setCheck2(true);
+        }, time * 1000);
       };
     }, time * 1000);
   }, [time, check, setCheck]);
@@ -37,16 +51,43 @@ function FireBall({ time = 1, rnd }) {
             fill="transparent"
           ></path>
         </>
-      ) : (
-        <rect x={"0px"} y={"10px"} height={"10px"} width={"10px"} fill={"red"}>
-          <animate
-            attributeName="rx"
-            values="0;5;0"
-            dur="10s"
-            repeatCount="definite"
-          />
-        </rect>
-      )}
+      ) : null}
+      {check && !check2 ? (
+        <>
+          {ptc.map((x, i) => {
+            let rx = 50 * Math.cos(x),
+              ry = 50 * Math.sin(x);
+
+            return (
+              <svg key={i}>
+                <circle
+                  cx={window.innerWidth * 0.5}
+                  cy={window.innerHeight * (0.4 - rnd * 0.2)}
+                  // cx={0}
+                  // cy={0}
+                  r={3}
+                  fill={"red"}
+                >
+                  <animateMotion
+                    dur={`${time}s`}
+                    repeatCount="definite"
+                    path={`M 0 0 L ${rx} ${-ry}`}
+                  />
+                </circle>
+                {/* <path
+                  d={`M ${window.innerWidth * 0.5} ${
+                    window.innerHeight * (0.4 - rnd * 0.2)
+                  } L ${window.innerWidth * 0.5 + rx} ${
+                    window.innerHeight * (0.4 - rnd * 0.2) - ry
+                  }`}
+                  stroke="black"
+                  fill="transparent"
+                ></path> */}
+              </svg>
+            );
+          })}
+        </>
+      ) : null}
     </svg>
   );
 }
